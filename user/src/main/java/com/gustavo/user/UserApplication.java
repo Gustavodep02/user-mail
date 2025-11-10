7a88a240a790337294199645c6c67e968e55d3ae
Gustavo <47760959+Gustavodep02@users.noreply.github.com>
2025-11-10 16:20:12 -0300

    initialize email and user microservices

---
+++ user/src/main/java/com/gustavo/user/UserApplication.java
@@ -0,0 +1,14 @@
+package com.gustavo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
+//DB_USERNAME=postgres;DB_PASSWORD=gustavocj2
